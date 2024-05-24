993214d927a42fcf5a8828a7fb3ff32208d6131e
Eyvar <eyvar31@gmail.com>
2024-05-24 17:24:03 -0500

    feat: hero section + responsive, colors added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			color:{
+				'primary': '#EF8AFF',
+				'line': '#F6AFFC',
+				'card-one': '#BDFBFF',
+				'card-two': '#FFF7B2',
+				'subtitle': '#D061BE',
+				'card-three': '#F3A7FA',
+		
+			}
+			
+
+
+		},
+		
 	},
 	plugins: [],
 
